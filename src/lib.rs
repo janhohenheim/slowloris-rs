fea082b6154cd52ce7f31771cd206f12cab586e8
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-10-31 21:16:43 +0100

    Add stream enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,51 @@
 #![feature(conservative_impl_trait)]
-extern crate url;
 extern crate native_tls;
-use url::{Url, Host};
+extern crate url;
+use url::{Host, Url};
 
-use native_tls::TlsConnector;
-use std::io::{Read, Write};
+use native_tls::{TlsConnector, TlsStream};
+use std::io::{self, Read, Write};
 use std::net::TcpStream;
 
+
+enum Stream<S>
+where
+    S: Read + Write,
+{
+    Tls(TlsStream<S>),
+    Plain(S),
+}
+
+impl<S> Read for Stream<S>
+where
+    S: Read + Write,
+{
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self {
+            &mut Stream::Tls(ref mut stream) => stream.read(buf),
+            &mut Stream::Plain(ref mut stream) => stream.read(buf),
+        }
+    }
+}
+
+impl<S> Write for Stream<S>
+where
+    S: Read + Write,
+{
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self {
+            &mut Stream::Tls(ref mut stream) => stream.write(buf),
+            &mut Stream::Plain(ref mut stream) => stream.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match self {
+            &mut Stream::Tls(ref mut stream) => stream.flush(),
+            &mut Stream::Plain(ref mut stream) => stream.flush(),
+        }
+    }
+}
 
 pub fn do_loris(url: &str) {
     let url = Url::parse(url).unwrap();
@@ -29,24 +68,23 @@ pub fn do_loris(url: &str) {
     assert!(!url.cannot_be_a_base());
     */
 
-    let stream = get_stream(&url);
+    let mut stream = get_stream(&url);
     stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
     let mut res = vec![];
     stream.read_to_end(&mut res).unwrap();
     println!("{}", String::from_utf8_lossy(&res));
-
 }
 
 
-fn get_stream<T>(url: &Url) -> Box<ReadWrite> {
+fn get_stream(url: &Url) -> Stream<TcpStream> {
     let domain = url.host_str().unwrap();
     let port = url.port_or_known_default().unwrap();
     let stream = TcpStream::connect((domain, port)).unwrap();
     if url.scheme() == "https" {
         let connector = TlsConnector::builder().unwrap().build().unwrap();
-        Box::new(connector.connect(domain, stream).unwrap())
+        Stream::Tls(connector.connect(domain, stream).unwrap())
     } else {
-        Box::new(stream)
+        Stream::Plain(stream)
     }
 }
 
